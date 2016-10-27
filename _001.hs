7f5c6ba54635a359d82149e03697aa9f81d865c0
kammitama5 <krystal.maughan@gmail.com>
2016-10-27 09:11:32 -0700

    something is wrong with this. Working on it

---
+++ _001.hs
@@ -0,0 +1,43 @@
+--1st value
+let x = map(+100000)[10000..99999]
+
+--2nd value map * 10 + 1
+let a = map(*10)[10000..99999]
+let y = map(+1)a
+
+--1st value * 3
+let b = map(*3)x
+
+
+	
+import Data.List	
+--find common element	
+	y `intersect` b
+
+--there is something wrong in this algorithm	
+-----------------------------
+
+----tests	
+	
+--w = y when equal to 3x
+--2nd value y == b
+--let z =
+	--if y == b 
+	--then z 
+	--else 0
+--let w =
+	--if y == f
+	--then w
+	--else 0
+	
+	
+	
+--let c = map(+10)[1..10]
+--let d = map(*10)[1..10]
+--let e = map(+1)d
+--let f = map(*3)c
+
+--f `intersect` c
+
+
+	
